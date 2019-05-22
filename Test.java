19cf523b26a2b10ee9c74ffcc191fede6ae607e8
drewszentesy@yahoo.com <drewszentesy@yahoo.com>
2019-05-22 14:57:50 -0500

    test changes

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.event.*;
-
+import java.awt.Graphics;
 public class Test extends JApplet {
   public void init(){
     JButton button = new JButton("Boop");
@@ -9,4 +9,7 @@ public class Test extends JApplet {
   public void start() {
   
   }
+  public void paint(Graphics g){  
+g.drawString("welcome",150,150);  
+  }  
 }
