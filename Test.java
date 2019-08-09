528f8f5935d108c921ae0edc692dfb18f4617ec5
Drew-11 <35682017+Drew-11@users.noreply.github.com>
2019-08-09 05:02:32 -0500

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.Graphics;
-public class Test extends JApplet {
-  public void paint(Graphics g){  
-g.drawString("welcome",150,150);  
-  }  
-}
